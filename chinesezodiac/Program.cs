5a566bb17ce6994ac88310bb2c5cd0fc6b792470
inuse918 <s2018s34@e-mirim.hs.kr>
2020-05-06 13:43:21 +0900

    실습예제 5 - 태어난 년도로 그 해의 띠 출력

--- chinesezodiac/Program.cs
+++ chinesezodiac/Program.cs
@@ -10,6 +10,51 @@ namespace chinesezodiac
     {
         static void Main(string[] args)
         {
+            Console.Write("태어난 년도를 입력하세요: ");
+            int year = int.Parse(Console.ReadLine());
+
+            switch (year %12)
+            {
+                case 0:
+                    Console.WriteLine("원숭이 띠 입니다.");
+                    break;
+                case 1:
+                    Console.WriteLine("닭 띠 입니다.");
+                    break;
+                case 2:
+                    Console.WriteLine("개 띠 입니다.");
+                    break;
+                case 3:
+                    Console.WriteLine("돼지 띠 입니다.");
+                    break;
+                case 4:
+                    Console.WriteLine("쥐 띠 입니다.");
+                    break;
+                case 5:
+                    Console.WriteLine("소 띠 입니다.");
+                    break;
+                case 6:
+                    Console.WriteLine("호랑이 띠 입니다.");
+                    break;
+                case 7:
+                    Console.WriteLine("토끼 띠 입니다.");
+                    break;
+                case 8:
+                    Console.WriteLine("용 띠 입니다.");
+                    break;
+                case 9:
+                    Console.WriteLine("뱀 띠 입니다.");
+                    break;
+                case 10:
+                    Console.WriteLine("말 띠 입니다.");
+                    break;
+                case 11:
+                    Console.WriteLine("양 띠 입니다.");
+                    break;
+                default:
+                    Console.WriteLine("헉~ 말도안돼~");
+                    break;
+            }
         }
     }
 }
